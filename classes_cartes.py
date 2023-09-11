31528d050e9fdfc6d028b900b896c617e6b0a06f
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-09-11 15:28:20 +0200

    Update classes_cartes.py

--- classes_cartes.py
+++ classes_cartes.py
@@ -1,14 +1,15 @@
-import random
-
+from random import shuffle
+from listes import *
+from piles import *
 
 class Carte:
-    COULEURS = ["trèfle", "coeur", "carreau", "pique"]
-    HAUTEURS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"]
-    COULEUR_SYMBOLS = {"trèfle": "♣", "coeur": "♥", "carreau": "♦", "pique": "♠"}
+    valides_hauteurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"]
+    valides_couleurs = ["coeur", "carreau", "trefle", "pique"]
+    symboles_couleurs = {"coeur": "♥", "carreau": "♦", "trefle": "♣", "pique": "♠"}
 
     def __init__(self, hauteur, couleur):
-        if hauteur not in self.HAUTEURS or couleur not in self.COULEURS:
-            raise ValueError("Hauteur ou couleur non valide.")
+        if hauteur not in self.valides_hauteurs or couleur not in self.valides_couleurs:
+            raise ValueError("Hauteur ou couleur non valide")
         self._hauteur = hauteur
         self._couleur = couleur
 
@@ -19,67 +20,62 @@ class Carte:
         return self._couleur
 
     def __str__(self):
-        return f"{self._hauteur} {self.COULEUR_SYMBOLS[self._couleur]}"
+        return f"{self._hauteur} de {self._couleur}"
 
     def __repr__(self):
         return f'Carte("{self._hauteur}", "{self._couleur}")'
 
-
 class FileDouble:
     def __init__(self):
-        self.pioche = []
-        self.defausse = []
+        self.pioche = Pile()
+        self.defausse = Pile()
 
-    def enfiler(self, carte):
-        self.pioche.append(carte)
+    def enfiler(self, valeur):
+        self.defausse.empiler(valeur)
 
     def defiler(self):
-        if not self.defausse:
-            if not self.pioche:
-                raise Exception("Les deux piles sont vides.")
-            while self.pioche:
-                self.defausse.append(self.pioche.pop())
-        return self.defausse.pop()
-
+        if self.pioche.est_vide():
+            while not self.defausse.est_vide():
+                self.pioche.empiler(self.defausse.dépiler())
+        return self.pioche.dépiler() if not self.pioche.est_vide() else None
 
 class Paquet:
     def __init__(self):
         self.file = FileDouble()
-        for couleur in Carte.COULEURS:
-            for hauteur in Carte.HAUTEURS:
+        for couleur in Carte.valides_couleurs:
+            for hauteur in Carte.valides_hauteurs:
                 self.file.enfiler(Carte(hauteur, couleur))
 
     def melange(self):
-        temp = self.file.pioche + self.file.defausse
-        random.shuffle(temp)
-        self.file.pioche, self.file.defausse = temp, []
+        cartes = []
+        while True:
+            carte = self.file.defiler()
+            if carte is None:
+                break
+            cartes.append(carte)
+        shuffle(cartes)
+        for carte in cartes:
+            self.file.enfiler(carte)
 
     def distribue(self, n):
         return [self.file.defiler() for _ in range(n)]
 
     def affichages(self):
-        return {
-            "pioche": [str(carte) for carte in self.file.pioche],
-            "defausse": [str(carte) for carte in self.file.defausse],
-        }
-
+        return f"Pioche: {[str(carte) for carte in self.file.pioche.liste_chainee]}, Defausse: {[str(carte) for carte in self.file.defausse.liste_chainee]}"
 
 class Main:
     def __init__(self, cartes):
         self.cartes = cartes
 
-    def tri(self, decroissant=False, as_faible=False):
-        def cle_tri(carte):
-            hauteur_value = Carte.HAUTEURS.index(carte.hauteur())
-            if as_faible and carte.hauteur() == "as":
-                hauteur_value = -1
-            return hauteur_value, Carte.COULEURS.index(carte.couleur())
+    def trier(self, desc=False, as_fort=True):
+        self.cartes.sort(key=lambda carte: (Carte.valides_hauteurs.index(carte.hauteur()), Carte.valides_couleurs.index(carte.couleur())), reverse=desc)
+        if not as_fort:
+            as_carte = [carte for carte in self.cartes if carte.hauteur() == "as"]
+            autres_cartes = [carte for carte in self.cartes if carte.hauteur() != "as"]
+            self.cartes = autres_cartes + as_carte
 
-        self.cartes.sort(key=cle_tri, reverse=decroissant)
-
-    def affichage(self):
-        return " ".join(str(carte) for carte in self.cartes)
-
+    def afficher(self):
+        return ", ".join(str(carte) for carte in self.cartes)
 
 class Joueur:
     def __init__(self, pseudo):
@@ -87,22 +83,10 @@ class Joueur:
         self.score = 0
         self.main = None
 
-    def assigner_main(self, cartes):
-        self.main = Main(cartes)
-
     def jouer(self):
-        carte = input(f"{self.pseudo}, choisissez une carte de votre main: {self.main.affichage()}\n")
-        # Validation de la carte et autres fonctionnalités peuvent être ajoutées ici.
-        return carte
-
+        carte_choisie = input(f"{self.pseudo}, choisissez une carte à jouer (0-{len(self.main.cartes) - 1}): ")
+        return self.main.cartes.pop(int(carte_choisie))
 
 class JoueurIA(Joueur):
     def jouer_automatiquement(self):
-        # Implémentation basique : choisir une carte au hasard.
-        return random.choice(self.main.cartes)
-
-
-if __name__ == "__main__":
-    # Pour les tests
-    carte = Carte("as", "trèfle")
-    print(carte)
+        return self.main.cartes.pop()
