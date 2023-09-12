d612e2cb8c33abb0db2b8df9a8aab4daa0699466
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-09-12 20:38:21 +0200

    Add files via upload

---
+++ Fichiers_python/classes_black_jack.py
@@ -0,0 +1,41 @@
+from listes import *
+from piles import *
+from classes_cartes import *
+
+class BlackJack:
+    def __init__(self, joueur, croupier):
+        self.joueur = joueur
+        self.croupier = croupier
+        self.paquet = Paquet()
+        self.paquet.melange()
+
+    def distribuer_cartes_initiales(self):
+        for _ in range(2):  # Distribuer 2 cartes à chaque joueur
+            self.joueur.main.enfiler(self.paquet.file.defiler())
+            self.croupier.main.enfiler(self.paquet.file.defiler())
+
+    def jouer(self):
+        self.paquet.melange()
+        self.joueur.main = Main(self.paquet.distribue(2))
+        self.croupier.main = Main([self.paquet.file.defiler()])
+
+        while True:
+            print(f"Main de {self.joueur.pseudo}: {self.joueur.main.afficher()}")
+            action = input(f"{self.joueur.pseudo}, voulez-vous tirer (t) ou rester (r)? ")
+            if action == 't':
+                carte = self.paquet.file.defiler()
+                print(f"{self.joueur.pseudo} a tiré {carte}.")
+                self.joueur.main.enfiler(carte)
+                
+                score_joueur = self.joueur.calculer_score()  # calcul du score après avoir tiré une carte
+                if score_joueur > 21:
+                    print(f"{self.joueur.pseudo} dépasse 21. C'est une défaite!")
+                    return
+            else:
+                break  # le joueur a choisi de "rester"
+
+        while self.croupier.calculer_score() < 17:  # Le croupier tire tant que son score est inférieur à 17
+            carte = self.paquet.file.defiler()
+            print(f"Croupier a tiré {carte}.")
+            self.croupier.main.enfiler(carte)
+            # la mise à jour du score du croupier se fait automatiquement à chaque tour de boucle avec calculer_score()
---
+++ Fichiers_python/classes_cartes_black_jack.py
@@ -0,0 +1,131 @@
+from random import shuffle
+from listes import *
+from piles import *
+
+class Carte:
+    valides_hauteurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"]
+    valides_couleurs = ["coeur", "carreau", "trefle", "pique"]
+    symboles_couleurs = {"coeur": "♥", "carreau": "♦", "trefle": "♣", "pique": "♠"}
+
+    def __init__(self, hauteur, couleur):
+        if hauteur not in self.valides_hauteurs or couleur not in self.valides_couleurs:
+            raise ValueError("Hauteur ou couleur non valide")
+        self._hauteur = hauteur
+        self._couleur = couleur
+
+    def hauteur(self):
+        return self._hauteur
+
+    def couleur(self):
+        return self._couleur
+
+    def __str__(self):
+        return f"{self._hauteur} de {self._couleur}"
+
+    def __repr__(self):
+        return f'Carte("{self._hauteur}", "{self._couleur}")'
+
+    def valeur(self):
+        if self._hauteur in ["roi", "dame", "valet"]:
+            return 10
+        elif self._hauteur == "as":
+            return 11
+        else:
+            return int(self._hauteur)
+
+class FileDouble:
+    def __init__(self):
+        self.pioche = Pile()
+        self.defausse = Pile()
+
+    def enfiler(self, valeur):
+        self.defausse.empiler(valeur)
+
+    def defiler(self):
+        if self.pioche.est_vide():
+            while not self.defausse.est_vide():
+                self.pioche.empiler(self.defausse.dépiler())
+        return self.pioche.dépiler() if not self.pioche.est_vide() else None
+
+class Paquet:
+    def __init__(self):
+        self.file = FileDouble()
+        for couleur in Carte.valides_couleurs:
+            for hauteur in Carte.valides_hauteurs:
+                self.file.enfiler(Carte(hauteur, couleur))
+
+    def melange(self):
+        cartes = []
+        while True:
+            carte = self.file.defiler()
+            if carte is None:
+                break
+            cartes.append(carte)
+        shuffle(cartes)
+        for carte in cartes:
+            self.file.enfiler(carte)
+
+    def distribue(self, n):
+        return [self.file.defiler() for _ in range(n)]
+
+    def affichages(self):
+        return f"Pioche: {[str(carte) for carte in self.file.pioche.liste_chainee]}, Defausse: {[str(carte) for carte in self.file.defausse.liste_chainee]}"
+
+class Main:
+    def __init__(self, cartes=[]):
+        self.cartes = cartes
+
+    def trier(self, desc=False, as_fort=True):
+        self.cartes.sort(key=lambda carte: (Carte.valides_hauteurs.index(carte.hauteur()), Carte.valides_couleurs.index(carte.couleur())), reverse=desc)
+        if not as_fort:
+            as_carte = [carte for carte in self.cartes if carte.hauteur() == "as"]
+            autres_cartes = [carte for carte in self.cartes if carte.hauteur() != "as"]
+            self.cartes = autres_cartes + as_carte
+
+    def enfiler(self, carte):
+        self.cartes.append(carte)
+
+    def defiler(self):
+        if self.cartes:
+            return self.cartes.pop(0)
+        return None
+
+    def est_vide(self):
+        return len(self.cartes) == 0
+
+    def afficher(self):
+        return ", ".join(str(carte) for carte in self.cartes)
+
+
+class Joueur:
+    def __init__(self, pseudo):
+        self.pseudo = pseudo
+        self.main = Main([])
+
+    def calculer_score(self):
+        score = sum(carte.valeur() for carte in self.main.cartes)
+        nombre_as = sum(1 for carte in self.main.cartes if carte.hauteur() == "as")
+        while score > 21 and nombre_as:
+            score -= 10  # Convertir un As de 11 points à 1 point
+            nombre_as -= 1
+        return score
+
+    def jouer(self):
+        if not self.main.cartes:
+            print(f"{self.pseudo}, vous n'avez pas de cartes en main.")
+            return None
+        carte_choisie = input(f"{self.pseudo}, choisissez une carte à jouer (0-{len(self.main.cartes) - 1}): ")
+        return self.main.cartes.pop(int(carte_choisie))
+
+
+
+class JoueurIA(Joueur):
+    def jouer(self):
+        if not self.main.cartes:
+            print("Erreur: Pas de cartes dans la main.")
+            return None
+        return self.main.cartes.pop()
+
+
+
+# Vous pouvez maintenant tester le code en créant différentes instances de ces classes et en exécutant les méthodes correspondantes.
