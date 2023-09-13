dadb9a987a9a310a8e164787d9cef52fc1dda745
LunaGrandjean <125506519+LunaGrandjean@users.noreply.github.com>
2023-09-13 11:51:54 +0200

    Update classes_black_jack.py

--- Fichiers_python/classes_black_jack.py
+++ Fichiers_python/classes_black_jack.py
@@ -1,6 +1,6 @@
 from listes import liste_vide, cellule, tête, queue, est_vide
 from piles import Pile
-from classes_cartes_black_jack import Paquet, Main, Joueur
+from classes_cartes_black_jack import Paquet, Main, Joueur, Joueur
 
 class BlackJack:
     def __init__(self, joueur, croupier):
