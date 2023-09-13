a0b33538e2f4cda7d39783d563cd71675bb24038
Loloeny <125599902+Loloeny@users.noreply.github.com>
2023-09-13 17:08:05 +0200

    Update classes_cartes_bataille.py

--- Fichiers_python/classes_cartes_bataille.py
+++ Fichiers_python/classes_cartes_bataille.py
@@ -1,38 +1,48 @@
 from random import shuffle
+# Importation des fonctionnalités nécessaires depuis les autres modules.
 from listes import liste_vide, cellule, tête, queue, est_vide
 from piles import Pile
 
 class Carte:
-# Classe représentant une carte de jeu.
-# Elle a des valeurs de hauteur, de couleur, et une méthode pour afficher la carte sous forme de chaîne de caractères.
+    # Classe pour représenter une carte individuelle dans un jeu de cartes standard.
     
+    # Les hauteurs et les couleurs valides pour toutes les cartes.
     valides_hauteurs = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "valet", "dame", "roi", "as"]
     valides_couleurs = ["coeur", "carreau", "trefle", "pique"]
+    
+    # Symboles pour représenter visuellement chaque couleur.
     symboles_couleurs = {"coeur": "♥", "carreau": "♦", "trefle": "♣", "pique": "♠"}
 
     def __init__(self, hauteur, couleur):
+        # Initialisation d'une carte avec une hauteur et une couleur spécifiées.
+        # Si la hauteur ou la couleur ne sont pas valides, une erreur est levée.
+        
         if hauteur not in self.valides_hauteurs or couleur not in self.valides_couleurs:
             raise ValueError("Hauteur ou couleur non valide")
         self._hauteur = hauteur
         self._couleur = couleur
 
     def hauteur(self):
+        # Renvoie la hauteur de la carte.
         return self._hauteur
 
     def couleur(self):
+        # Renvoie la couleur de la carte.
         return self._couleur
 
     def __str__(self):
+        # Représentation sous forme de chaîne de caractères pour l'affichage.
         return f"{self._hauteur} de {self._couleur}"
 
     def __repr__(self):
+        # Représentation formelle pour la carte.
         return f'Carte("{self._hauteur}", "{self._couleur}")'
 
 class FileDouble:
-# Classe représentant une file double utilisée pour gérer les cartes.
-# Elle a une pioche et une défausse pour gérer les cartes.
- 
+    # Une structure de données qui utilise deux piles pour implémenter une file.
+    
     def __init__(self):
+        # Initialisation avec deux piles vides : pioche et défausse.
         self.pioche = Pile()
         self.defausse = Pile()
 
@@ -41,7 +51,7 @@ class FileDouble:
         self.defausse.empiler(valeur)
 
     def defiler(self):
-        # Récupère une carte de la pioche ou la récupère de la défausse si la pioche est vide.
+        # Récupère une valeur de la pioche. Si la pioche est vide, renverse la défausse dans la pioche.
         
         if self.pioche.est_vide():
             while not self.defausse.est_vide():
@@ -49,17 +59,18 @@ class FileDouble:
         return self.pioche.dépiler() if not self.pioche.est_vide() else None
 
 class Paquet:
-    # Classe représentant un paquet de cartes.
-    # Elle génère un paquet complet de cartes, les mélange et les distribue.
+    # Classe pour représenter un jeu complet de cartes.
     
     def __init__(self):
+        # Crée un paquet complet de cartes et les ajoute à la file.
         self.file = FileDouble()
         for couleur in Carte.valides_couleurs:
             for hauteur in Carte.valides_hauteurs:
                 self.file.enfiler(Carte(hauteur, couleur))
 
     def melange(self):
-        # Mélange le paquet de cartes.
+        # Mélange le paquet de cartes en utilisant la fonction shuffle.
+        
         cartes = []
         while True:
             carte = self.file.defiler()
@@ -71,54 +82,60 @@ class Paquet:
             self.file.enfiler(carte)
 
     def distribue(self, n):
-        # Distribue n cartes du paquet.
-
+        # Distribue les n premières cartes du paquet.
         return [self.file.defiler() for _ in range(n)]
 
     def affichages(self):
-        # Affiche l'état actuel de la pioche et de la défausse.
-
+        # Retourne une représentation sous forme de chaîne de caractères de la pioche et de la défausse.
         return f"Pioche: {[str(carte) for carte in self.file.pioche.liste_chainee]}, Defausse: {[str(carte) for carte in self.file.defausse.liste_chainee]}"
 
 class Main:
-# Classe représentant la main d'un joueur.
-# Elle peut trier les cartes, ajouter une carte à la main ou retirer une carte de la main.
-
+    # Classe pour représenter la main d'un joueur.
+    
     def __init__(self, cartes=[]):
+        # Initialisation avec une liste de cartes.
         self.cartes = cartes
 
     def trier(self, desc=False, as_fort=True):
+        # Trie les cartes en fonction de leur hauteur et couleur.
+        
         self.cartes.sort(key=lambda carte: (Carte.valides_hauteurs.index(carte.hauteur()), Carte.valides_couleurs.index(carte.couleur())), reverse=desc)
+        # Si l'As doit être considéré comme la plus faible carte, déplacez-le à la fin.
         if not as_fort:
             as_carte = [carte for carte in self.cartes if carte.hauteur() == "as"]
             autres_cartes = [carte for carte in self.cartes if carte.hauteur() != "as"]
             self.cartes = autres_cartes + as_carte
 
     def enfiler(self, carte):
+        # Ajoute une carte à la main.
         self.cartes.append(carte)
 
     def defiler(self):
+        # Retire et retourne la première carte de la main.
         if self.cartes:
             return self.cartes.pop(0)
         return None
 
     def est_vide(self):
+        # Vérifie si la main est vide.
         return len(self.cartes) == 0
 
     def afficher(self):
+        # Retourne une représentation sous forme de chaîne de caractères de toutes les cartes dans la main.
         return ", ".join(str(carte) for carte in self.cartes)
 
-
 class Joueur:
-# Classe représentant un joueur.
-# Elle a un pseudo, un score, une main de cartes et une méthode pour jouer une carte.
+    # Classe pour représenter un joueur humain.
     
     def __init__(self, pseudo):
+        # Initialisation avec un pseudo, un score initialisé à 0 et une main vide.
         self.pseudo = pseudo
         self.score = 0
         self.main = Main([])
 
     def jouer(self):
+        # Demande au joueur de choisir une carte à jouer.
+        
         if not self.main.cartes:
             print(f"{self.pseudo}, vous n'avez pas de cartes en main.")
             return None
@@ -126,10 +143,11 @@ class Joueur:
         return self.main.cartes.pop(int(carte_choisie))
 
 class JoueurIA(Joueur):
-# Classe représentant un joueur IA.
-# Elle hérite des fonctionnalités du joueur standard et joue automatiquement en retirant la carte du haut.
+    # Classe pour représenter un joueur géré par l'IA.
     
     def jouer(self):
+        # L'IA joue automatiquement en retirant la carte du haut.
+        
         if not self.main.cartes:
             print("Erreur: Pas de cartes dans la main.")
             return None
